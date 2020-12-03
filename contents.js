89387d6e6b89e42954e462b17dc049308fc69723
Amirkhan9439 <amirk9439@gmail.com>
2020-12-03 15:48:51 +0500

    clear

--- contents.js
+++ contents.js
@@ -1,4 +1,4 @@
 var arr = [["1","3 December, 2020","Saturday", "OOP", "http"],
 		   ["2",  "4 December, 2020","Sunday", "OOP","http"],
 		   ["2",  "4 December, 2020","Sunday", "OOP","http"],
-		   ["2",  "4 December, 2020","Sunday", "OOP Inheritcance and polymorphism","http"]];
+		   ["2",  "4 December, 2020","Sunday", "OOP by Aamir and polymorphism","http"]];
