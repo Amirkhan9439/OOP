5a21319982c8201a871a98e5ca33602c82cb644d
Amirkhan9439 <amirk9439@gmail.com>
2020-12-05 12:43:19 +0500

    Lec

--- contents.js
+++ contents.js
@@ -1,6 +1,1 @@
-var arr = [["1","3 December, 2020","Saturday", "OOP", "testing"],
-		   ["2",  "4 December, 2020","Sunday", "OOP","https://www.w3schools.com"],
-		   ["2",  "4 December, 2020","Sunday", "OOP","http"],
-		   ["2",  "4 December, 2020","Sunday", "OOP by Aamir and polymorphism","http"],
-		   ["19", "3 December, 2020", "Thursday", "Testing", "meet.google.com"],
-		   ["420", "3 December, 2020", "Thursday", "please", "final"]];
+var arr = [["1","5 December, 2020","Saturday", "Revision of functions, structures, Intro to OOP, classes and objects", "https://drive.google.com/file/d/1RS4HkpjCirQaSX3EwAH2ynBfCTAhk1xX/view?usp=sharing"]];
