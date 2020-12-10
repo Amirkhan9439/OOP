a14ea6cf0b4fc5671b51ebfe7e4849b68430663d
Amirkhan9439 <amirk9439@gmail.com>
2020-12-10 13:35:09 +0500

    Lec3

--- contents.js
+++ contents.js
@@ -1,2 +1,3 @@
 var arr = [["1","5 December, 2020","Saturday", "Revision of functions, structures, Intro to OOP, classes and objects", "https://drive.google.com/file/d/1RS4HkpjCirQaSX3EwAH2ynBfCTAhk1xX/view?usp=sharing"],
-		   ["2", "6 December, 2020", "Sunday", "Constructors, objects as arguments", "https://drive.google.com/file/d/1fdYdKy8IIuxE5wrnBoJFlG03_HI338yb/view?usp=sharing"]];
+		   ["2", "6 December, 2020", "Sunday", "Constructors, objects as arguments", "https://drive.google.com/file/d/1fdYdKy8IIuxE5wrnBoJFlG03_HI338yb/view?usp=sharing"],
+		   ["3", "10 December, 2020", "Thursday", "Access-specifiers, this pointer, inheritance (intro)", "https://drive.google.com/file/d/1HyKlLgnQT7HfegEa2sfuHDZi9ANIU6-r/view?usp=sharing"]];
