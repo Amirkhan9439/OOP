a777751e50d77e833c1b825bcd4b0b931f0fd7dc
Amirkhan9439 <amirk9439@gmail.com>
2020-12-11 11:48:51 +0500

    lec4

--- contents.js
+++ contents.js
@@ -1,3 +1,4 @@
 var arr = [["1","5 December, 2020","Saturday", "Revision of functions, structures, Intro to OOP, classes and objects", "https://drive.google.com/file/d/1RS4HkpjCirQaSX3EwAH2ynBfCTAhk1xX/view?usp=sharing"],
 		   ["2", "6 December, 2020", "Sunday", "Constructors, objects as arguments", "https://drive.google.com/file/d/1fdYdKy8IIuxE5wrnBoJFlG03_HI338yb/view?usp=sharing"],
-		   ["3", "10 December, 2020", "Thursday", "Access-specifiers, this pointer, inheritance (intro)", "https://drive.google.com/file/d/1HyKlLgnQT7HfegEa2sfuHDZi9ANIU6-r/view?usp=sharing"]];
+		   ["3", "10 December, 2020", "Thursday", "Access-specifiers, this pointer, inheritance (intro)", "https://drive.google.com/file/d/1HyKlLgnQT7HfegEa2sfuHDZi9ANIU6-r/view?usp=sharing"],
+		   ["4", "11 December, 2020", "Friday", "Static members, multiple inheritances and function overriding (intro)", "https://drive.google.com/file/d/1nK55-JvVkRkZ1SAIBCuFW5On2tRBg--Z/view?usp=sharing"]];
