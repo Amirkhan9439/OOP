8517d998f073b2a60b506d088174e0d7cb9da299
Amirkhan9439 <amirk9439@gmail.com>
2020-12-26 13:09:02 +0500

    lec6

--- contents.js
+++ contents.js
@@ -2,4 +2,5 @@ var arr = [["1","5 December, 2020","Saturday", "Revision of functions, structure
 		   ["2", "6 December, 2020", "Sunday", "Constructors, objects as arguments", "https://drive.google.com/file/d/1fdYdKy8IIuxE5wrnBoJFlG03_HI338yb/view?usp=sharing"],
 		   ["3", "10 December, 2020", "Thursday", "Access-specifiers, this pointer, inheritance (intro)", "https://drive.google.com/file/d/1HyKlLgnQT7HfegEa2sfuHDZi9ANIU6-r/view?usp=sharing"],
 		   ["4", "11 December, 2020", "Friday", "Static members, multiple inheritances and function overriding (intro)", "https://drive.google.com/file/d/1nK55-JvVkRkZ1SAIBCuFW5On2tRBg--Z/view?usp=sharing"],
-		   ["5", "24 December, 2020", "Thursday", "Inheritance, operators' overlaoding", "https://drive.google.com/file/d/1j_HLAdcl5MLkzq0_PQh_5vF845Q3DRf5/view?usp=sharing"]];
+		   ["5", "24 December, 2020", "Thursday", "Inheritance, operators' overlaoding", "https://drive.google.com/file/d/1j_HLAdcl5MLkzq0_PQh_5vF845Q3DRf5/view?usp=sharing"],
+		   ["6", "26 December, 2020", "Saturday", "Operators' overloading,unary and binary", "https://drive.google.com/file/d/1q-7f7WZF6S2Lm6b3eieOgkYX6jsyM_Og/view?usp=sharing"]];
